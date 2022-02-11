577bcf44bda144bce0ce739058abb5f6515ee4cd
sonarAIT <sonarmikumiku@gmail.com>
2022-02-11 22:32:07 +0900

    delete: 未使用のAPIを削除

--- src/chsp-main.ts
+++ src/chsp-main.ts
@@ -1,25 +1,15 @@
 import { Plugin } from "obsidian";
-import pinyin from "pinyinlite";
 import TinySegmenter from "tiny-segmenter/lib/index";
 
-import { API_NAME, ChsPatchAPI, Evt_ApiReady } from "./api";
 import patchGetWordAt from "./cm5";
 import { getChsPatchExtension, getWordAtPatchUnloader } from "./cm6/index";
 
-const API_NAME: API_NAME extends keyof typeof window ? API_NAME : never =
-  "ChsPatchAPI" as const; // this line will throw error when name out of sync
-const chsRegex = /[\u4e00-\u9fa5]/g;
-
 export default class CMChsPatch extends Plugin {
-  api?: ChsPatchAPI;
-
   tinySegmenter: ReturnType<typeof TinySegmenter>;
 
   async onload() {
-    console.log("loading cm-chs-patch");
-    this.tinySegmenter = new TinySegmenter()
-
-    this.loadApi();
+    console.log("loading cm-japanese-patch");
+    this.tinySegmenter = new TinySegmenter();
 
     // for cm5
     const cm5PatchUnloader = patchGetWordAt(this.tinySegmenter);
@@ -28,15 +18,5 @@ export default class CMChsPatch extends Plugin {
     // for cm6
     this.registerEditorExtension(getChsPatchExtension(this.tinySegmenter));
     this.register(getWordAtPatchUnloader(this.tinySegmenter));
-  }
-
-  loadApi() {
-    const api = { pinyin, chsRegex };
-    this.api = api;
-    window[API_NAME] = api;
-    this.register(() => (window[API_NAME] = undefined));
-    console.log("ChsPatch API ready");
-
-    this.app.metadataCache.trigger(...(<Evt_ApiReady>["chs-patch:ready", api]));
   }
 }
