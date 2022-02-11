558df11000b9d0d0851f5fc83d41d39ab907044c
sonarAIT <sonarmikumiku@gmail.com>
2022-02-11 22:04:42 +0900

    chore: modules.d.tsにtiny-segmenterを追加

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,11 +1,1 @@
-declare module "segmentit";
-declare module "pinyinlite" {
-  interface PinyinLiteOpts {
-    /** Whether to retain full-width characters and half-width punctuation marks that cannot be obtained in pinyin,
-     * the default is false, that is, not to retain, the corresponding position will be an empty array.
-     * Letters and numbers are always output as they are.  */
-    keepUnrecognized: boolean;
-  }
-  function getPinyin(str: string, opts: PinyinLiteOpts): string[][];
-  export default getPinyin;
-}
+declare module "tiny-segmenter/lib/index";
